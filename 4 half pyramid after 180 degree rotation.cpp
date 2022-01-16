6bc7d2d2548ba71fb8452c6b73f11aca33d30b4d
iamsolo023 <74610524+iamsolo023@users.noreply.github.com>
2022-01-17 01:07:49 +0530

    Add files via upload

---
+++ 4 half pyramid after 180 degree rotation.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n, i , j;
+    cout<<"Enter the value of n: "<<endl;
+    cin>>n;
+    cout<<endl;
+    cout<<"Your Half pyramid after 180 degree rotation is printed below."<<endl<<endl;
+
+    for(i=1; i<=n; i++){
+        for(j=1; j<=n; j++){
+            if(j<=(n-i)){
+                cout<<" ";
+            }
+            else{
+                cout<<"*";
+            }
+        }
+         cout<<endl;
+    }
+    return 0;
+}
